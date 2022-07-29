61a18b15440b1ef1664feb9adc508af02ca8ce9f
Matt Gates <matthew.gates2001@gmail.com>
2022-07-29 10:19:05 -0600

    Arrays exercise completed and filled out. Ready to submit.

--- arrays.js
+++ arrays.js
@@ -2,13 +2,16 @@
 /* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
 
 const faveColors = ['red', 'green', 'black']
+console.log(faveColors) // answer checker
 
 //CODE HERE
-
+const colorCopy = faveColors.slice(0, 3)
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push('blue')
+console.log(colorCopy) // answer checker
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,7 +22,8 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+const middleNums = numbers.slice(1, 4)
+console.log(middleNums) // answer checker
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +38,12 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+const answers = []
+for(let i = 0; i < bigOrSmallArray.length; i++){
+  if (bigOrSmallArray[i] > 100){
+    answers.push('big')
+  }else if(bigOrSmallArray[i] <= 100){
+    answers.push('small')
+  }
+}
+console.log(answers) // answer checker
