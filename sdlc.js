7a7e08f68b9c226bc9994b2c749a02f5fda39ba7
Matt Gates <matthew.gates2001@gmail.com>
2022-07-29 10:01:42 -0600

    Initial Commit

---
+++ sdlc.js
@@ -0,0 +1,37 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+
+// Planning - The planning phase encompasses all aspects of project and product management. This typically includes resource allocation, capacity planning, project scheduling, cost estimation, and provisioning.
+// Analysis of Requirements - The client has a vague idea of what is required. The sales department, software engineers and domain experts all collaborate to gather information about what the product should be, its purposes and how it is supposed to work. This analysis is often coupled by market surveys.
+// Design - During this phase, the overall system architecture is designed by defining the functionality of each module as well as their interaction with third-party modules.
+// Implementation - The product is built and coded according to the companys own standards. Designers follow a set of best practices and guidelines, and use the programming tools they have at their disposal to develop the product.
+// Testing & Integration - The code is tested to make sure it is free of bugs, adheres to the client requirements and correctly interfaces with other software products together with the underlying hardware.
+// Maintain - The final version of the product is released to the market. Maintenance is performed over time to change the system architecture according to future needs. This may require the addition of code or alteration of the existing code.
+
+
+
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements =  "The client has a vague idea of what is required. The sales department, software engineers and domain experts all collaborate to gather information about what the product should be, its purposes and how it is supposed to work. This analysis is often coupled by market surveys."
+const design = "During this phase, the overall system architecture is designed by defining the functionality of each module as well as their interaction with third-party modules."
+const implementation = "The product is built and coded according to the companys own standards. Designers follow a set of best practices and guidelines, and use the programming tools they have at their disposal to develop the product."
+const testingAndIntegration = "The code is tested to make sure it is free of bugs, adheres to the client requirements and correctly interfaces with other software products together with the underlying hardware."
+const maintain = "The final version of the product is released to the market. Maintenance is performed over time to change the system architecture according to future needs. This may require the addition of code or alteration of the existing code."
